69945e3aa44b6de0fca225d6a2f7b33f0e4da12a
GimGayeon <kky1373@naver.com>
2024-06-10 14:06:30 +0900

    배포

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/react-shopping-products/dist',
   plugins: [react({ jsxImportSource: '@emotion/react' })],
   resolve: {
     alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
